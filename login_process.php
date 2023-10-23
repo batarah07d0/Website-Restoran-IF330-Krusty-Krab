125e20b23cfaf8aa3e5af8890f29bb7cf23a93ff
Batara Hotdo <batarah07d0@gmail.com>
2023-10-23 18:57:56 +0700

    head

---
+++ login_process.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require_once ('db.php');
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Sanitize and validate user input
+$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+$sql = "SELECT * FROM account WHERE username = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$username]);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if(!$row){
+    // User Not Found
+    header('location: form_process.php?error-username=User not Found');
+} else {
+    if(!password_verify($password, $row['password'])){
+        // Wrong Password
+        header('location: form_process.php?error-password=Wrong Password&username=' . urlencode($username));
+       echo "Salah";
+    } else {
+        // Correct Password
+        $_SESSION['username'] = $row['username'];
+        header('location: index.php');
+    }
+}
+?>
